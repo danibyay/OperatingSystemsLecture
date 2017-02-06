c12faf78d9385e334f3b60b27e5a5ace2aee2aab
Daniela Becerra <madajiji@gmail.com>
2017-02-06 10:00:39 -0600

    add get pid syscall to hello world

--- labs/01/hello.c
+++ labs/01/hello.c
@@ -1,10 +1,13 @@
 /* Hello World program */
-
+#include <sys/types.h>
+#include <unistd.h>
 #include <stdio.h>
 
 int main(void) {
     printf("Hello World\n");
+    int ret = getpid();
     printf("My name is Daniela Becerra Gonzalez \n");
+    printf("pid is %d \n", ret);
     return 0;
 }
  // send to victor.rodriguez.bahena@itesm.mx
